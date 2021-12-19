b40171ff1fcfad0614fb74e6682d486e518bc357
Rootdiv <rootdiv@mail.ru>
2021-12-19 16:37:36 +0300

    fix: open and close modal window

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,12 +1,9 @@
-'use strict';
-
 const cart = () => {
-  const cartBtn = document.getElementById('cart');
+  //const cartBtn = document.getElementById('cart');
   const cartModal = document.querySelector('.cart');
-  const cartCloseModal = cartModal.querySelector('.cart-close');
+  //const cartCloseModal = cartModal.querySelector('.cart-close');
 
-  const openCart = event => {
-    event.preventDefault();
+  const openCart = () => {
     cartModal.style.display = 'flex';
   };
 
@@ -14,8 +11,18 @@ const cart = () => {
     cartModal.removeAttribute('style');
   };
 
-  cartBtn.addEventListener('click', openCart);
-  cartCloseModal.addEventListener('click', closeCart);
+  document.addEventListener('click', event => {
+    const target = event.target;
+    if (target.closest('#cart')) {
+      openCart();
+    } else if (target.closest('.cart-close')) {
+      closeCart();
+    } else if (target.matches('.cart') && !target.closest('.cart-body')) {
+      closeCart();
+    }
+  });
+  // cartBtn.addEventListener('click', openCart);
+  // cartCloseModal.addEventListener('click', closeCart);
 };
 
 export default cart;
